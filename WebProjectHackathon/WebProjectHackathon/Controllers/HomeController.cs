5e5c07f7e0e7fb538ec8aa58cfca99619dfd3410
Boris <boris.gluchinskiy@gmail.com>
2025-03-30 22:53:48 +0300

    Adding user pages

--- WebProjectHackathon/WebProjectHackathon/Controllers/HomeController.cs
+++ WebProjectHackathon/WebProjectHackathon/Controllers/HomeController.cs
@@ -2,8 +2,6 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using WebProjectHackathon.Models;
 
-namespace WebProjectHackathon.Controllers;
-
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _Intex;
---
+++ WebProjectHackathon/WebProjectHackathon/Controllers/PersonalityUserPagesController.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebProjectHackathon.Models;
+
+public class PersonalityUserPagesController : Controller
+{
+    private readonly ILogger<HomeController> _Intex;
+
+    public PersonalityUserPagesController(ILogger<HomeController> intex)
+    {
+        _Intex = intex;
+    }
+
+    public IActionResult Authorization()
+    {
+        return View();
+    }
+    public IActionResult PersonalOffice()
+    {
+        return View();
+    }
+    public IActionResult Registration()
+    {
+        return View();
+    }
+}
